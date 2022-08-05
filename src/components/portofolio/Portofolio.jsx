3a485ef4042c34b33722f8c3fb1bf2333726d13b
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2022-08-05 17:11:01 +0200

    ..

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -31,7 +31,7 @@ const Portofolio = () => {
                             filteredRepos.map(repo => {
                                 return (
                                     <div className="repo-card" key={repo.id}>
-                                        <h4 >{repo.name}</h4>
+                                        <h4 >{repo.name.replace("-html", "").replace("-js", "").replace("-css", "").replace("-bootstrap", "").replace("-scss", "").trim()}</h4>
                                         <img src={`https://raw.githubusercontent.com/mohammedkhaled15/${repo.name}/master/image.png`} alt="repo main img" />
                                     </div>
                                 )
