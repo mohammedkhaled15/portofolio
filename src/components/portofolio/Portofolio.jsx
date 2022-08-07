fbcc5495eec628424087bf546311fa85d07a78db
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2022-08-07 16:05:13 +0200

    framer Motion

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -2,18 +2,13 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import "./portofolio.css"
 import Filters from './Filters'
-// import image from "https://raw.githubusercontent.com/mohammedkhaled15/calculator-css-js/master/image.jpg"
+import { motion, AnimatePresence } from 'framer-motion'
 
 const Portofolio = () => {
     const [filteredRepos, setFilteredRepos] = useState(null)
     const [filterCriteria, setFilterCriteria] = useState("All")
     const [choosedRepos, setChoosedRepos] = useState([])
-    // const wordsToDeleteFromRepoName = {
-    //     html: "",
-    //     js: "",
-    //     css: "",
-    //     scss: ""
-    // }
+
     const deleteAddedWords = str => {
         const words = ["PS22-", "-html", "-css", "-scss", "-js", "-react", "-tailwind", "-bootstrap", "-oop"]
         for (let i in words) {
@@ -35,21 +30,23 @@ const Portofolio = () => {
             <h2>Portofolio</h2>
             <div className="container portofolio__container">
                 <Filters filteredRepos={filteredRepos} setChoosedRepos={setChoosedRepos} filterCriteria={filterCriteria} setFilterCriteria={setFilterCriteria} />
-                <div className="projects">
-                    {
-
-                        choosedRepos === null ? "Loading" : (
-                            choosedRepos.map(repo => {
-                                return (
-                                    <div className="repo-card" key={repo.id}>
-                                        <h4 >{deleteAddedWords(repo.name).replaceAll("-", " ")}</h4>
-                                        <img src={`https://raw.githubusercontent.com/mohammedkhaled15/${repo.name}/master/image.png`} alt="repo main img" />
-                                    </div>
-                                )
-                            })
-                        )
-                    }
-                </div>
+                <AnimatePresence>
+                    <motion.div layout className="projects">
+                        {
+                            choosedRepos === null ? "Loading" : (
+                                choosedRepos.map(repo => {
+                                    return (
+                                        <motion.div layout animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} transition={{ duration: 0.8 }}
+                                            className="repo-card" key={repo.id}>
+                                            <h4 >{deleteAddedWords(repo.name).replaceAll("-", " ")}</h4>
+                                            <img src={`https://raw.githubusercontent.com/mohammedkhaled15/${repo.name}/master/image.png`} alt="repo main img" />
+                                        </motion.div>
+                                    )
+                                })
+                            )
+                        }
+                    </motion.div>
+                </AnimatePresence>
             </div>
         </section>
     )
